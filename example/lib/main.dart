6715e652c0e1f6493e2fdd0781f385ee854542a8
Uchqun Shodmonov <shodmonov.uchqun@gmail.com>
2022-12-13 16:08:21 +0500

    version

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:pdf_flutter/pdf_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
